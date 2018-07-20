7e26dfd45055017f58ddb85b427033fa115c6060
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-07-20 02:07:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  
+  var emptyArray = []
+  for(let i =0;i<musicians.length;i++){
+    
+    var string = `${musicians[i]} plays ${instruments[i]}`
+    emptyArray.push(string)
+    
+  }
+  
+  return emptyArray
+}
